3cee9bbbf185e60a1d4a99c7bed586db816fcf81
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-05-15 10:56:33 -0600

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import './index.css';
-import React from 'react';
-import logo from './images/cat.jpg';
-  
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/navbar';
+
 function App() {
-    return (
-        <div>
-            <nav class="navbar background">
-                <ul class="nav-list">
-                    <div class="logo">
-                        <img className="logo" src= {logo}/>
-                    </div>
-                    <li><a href="#courses">Certifications</a></li>
-                    <li><a href="#tutorials">Projects</a></li>
-                    <li><a href="#jobs">About me </a></li>
-                    <li><a href='#student'>hi</a></li>
-                </ul>
-  
-                
-            </nav>
-  
-           
-           
-        </div>
-    )
+  return (
+    <div className="App">
+      
+     <h1>hello</h1>
+     <Navbar>
+        
+     </Navbar>
+
+    </div>
+  );
 }
+
 export default App;
